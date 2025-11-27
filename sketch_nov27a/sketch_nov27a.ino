792e9bcda2b0ac1e0ed6a331a699e187766acfcb
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-11-27 18:52:25 +0100

    first arduino program

---
+++ sketch_nov27a/sketch_nov27a.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  // delay(1000);
+  delay(100);
+  // delay(900);
+  digitalWrite(13, LOW);
+  // delay(1000);
+  delay(900);
+  // delay(100);
+}

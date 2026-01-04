04be2e0767d1b62b3091dd1fbe47071fbbd1f825
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2026-01-04 15:29:43 +0100

    understanding rgb leds

---
+++ 15_understanding_rgb_led/sketch_jan4b/sketch_jan4b.ino
@@ -0,0 +1,60 @@
+int redPin = 8;
+int greenPin = 9;
+int bluePin = 10;
+String myColor;
+String msg = "What Color Do You Want? ";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);  // Initialize serial communication
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(msg);
+  while (Serial.available() == 0) {
+
+  }
+  myColor = Serial.readString();
+  myColor.trim();  // Remove whitespace and newline characters
+
+  if (myColor == "red") {
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(bluePin, LOW);
+  }
+
+  if (myColor == "green") {
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(bluePin, LOW);
+  }
+
+  if (myColor == "blue") {
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(bluePin, HIGH);
+  }
+
+  if (myColor == "off") {
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(bluePin, LOW);
+  }
+
+  // rgb
+  if (myColor == "aqua") {
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, 255);
+    digitalWrite(bluePin, 255);
+  }
+
+  if (myColor == "purple") {
+    digitalWrite(redPin, 128);
+    digitalWrite(greenPin, 0);
+    digitalWrite(bluePin, 128);
+  }
+}

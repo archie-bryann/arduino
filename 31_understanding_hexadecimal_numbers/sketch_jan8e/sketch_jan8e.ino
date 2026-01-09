44cfe08393772fda9844ad6a91941a04787540c8
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2026-01-09 13:18:00 +0100

    serial to parallel shift register

---
+++ 31_understanding_hexadecimal_numbers/sketch_jan8e/sketch_jan8e.ino
@@ -0,0 +1,15 @@
+byte myByte = 0x0;
+int dt = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(myByte, HEX); 
+  myByte = myByte + 1;
+  delay(dt);
+}
+ 

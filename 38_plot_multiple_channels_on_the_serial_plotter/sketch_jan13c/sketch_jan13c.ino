4b6eb085d019b9961db5561a5ac3b617f3415c5b
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2026-01-13 15:21:27 +0100

    plot multiple channels on the serial plotter

---
+++ 38_plot_multiple_channels_on_the_serial_plotter/sketch_jan13c/sketch_jan13c.ino
@@ -0,0 +1,23 @@
+// int sinVal;
+// int cosVal;
+// int j;
+
+float sinVal;
+float cosVal;
+float j;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (j = 0; j <= 2*3.14159265; j = j + .01) {
+    sinVal = sin(j);
+    cosVal = cos(j);
+    Serial.print(sinVal);
+    Serial.print(",");
+    Serial.println(cosVal);
+  }
+}

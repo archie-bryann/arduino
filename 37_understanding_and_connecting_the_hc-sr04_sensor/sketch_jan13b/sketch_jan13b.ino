8cbaa3ff69b1a97aebc4569bb054b63cd6637049
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2026-01-13 15:01:52 +0100

    serial plotter

--- 37_understanding_and_connecting_the_hc-sr04_sensor/sketch_jan13b/sketch_jan13b.ino
+++ 37_understanding_and_connecting_the_hc-sr04_sensor/sketch_jan13b/sketch_jan13b.ino
@@ -1,22 +1,40 @@
 int trigPin = 12;
 int echoPin = 11;
-int pingTravelTime;
+
+long pingTravelTime;
+float distanceToTarget;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Send trigger pulse
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(10);
+  delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
+
+  // Read echo time in microseconds
   pingTravelTime = pulseIn(echoPin, HIGH);
+
+  // Convert time to distance (inches)
+  // distance = (time * speed of sound) / 2
+  distanceToTarget = (pingTravelTime * 0.0133) / 2;
+
+  Serial.print("Distance to Target is: ");
+  Serial.println(distanceToTarget);
+  Serial.println(" inches");
+
+  // Serial.print(0);
+  // Serial.print(",");
+  // Serial.print(distanceToTarget);
+  // Serial.print(",");
+  // Serial.println(10);
+
+
   delay(25);
-  Serial.println(pingTravelTime);
 }
